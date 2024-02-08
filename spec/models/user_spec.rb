5adcd7ed496153bb75679cf14c79f5cc9aa044b9
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-08 13:39:11 +0000

    [Finish] Rspec入門 #3

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a email and password" do
+    user = User.new(
+      email: "user1@user.com",
+      password: "user1pass",
+    )
+    expect(user).to be_valid
+  end
+end
