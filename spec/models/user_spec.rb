95dfde17284c8b32d11b32132cd6fa7a2e3dcda9
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-09 14:29:20 +0000

    [Finish] Rspec入門 #4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+
+  it "is invalid without an email" do
+    user = User.new(
+      email: nil,
+      password: "user1pass"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  
+  it "is invalid with a duplicate email address" do
+    User.create(
+      email: "user1@user.com",
+      password: "user1pass",
+    )
+    user = User.new(
+      email: "user1@user.com",
+      password: "user2pass",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
