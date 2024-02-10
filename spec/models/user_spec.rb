7f70c35c0cadec839b76c55f76b1155eed598f8a
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-10 13:42:47 +0000

    [Finish] Rspec入門 #6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it "is valid with a email and password" do
     user = User.new(
       email: "user1@user.com",
@@ -17,7 +18,7 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
-  
+
   it "is invalid with a duplicate email address" do
     User.create(
       email: "user1@user.com",
@@ -29,5 +30,10 @@ RSpec.describe User, type: :model do
     )
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
+  end
+
+    it "factorybot test" do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
   end
 end
