ae4901316a834752623b9d407cbceaa7635a9031
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-12 15:11:45 +0000

    [Finish] Rspec入門#8

---
+++ spec/requests/posts_request.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "[Action test]" do
+     before do
+      @user = FactoryBot.create(:user, :a)
+     end
+
+    context "new" do
+      it "access by user" do
+        sign_in @user
+        get '/posts/new'
+        expect(response).to be_successful
+      end
+    end
+  end
+end
