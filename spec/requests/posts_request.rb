f30e1e3feefff2ef18089b4e9d7f7cbcffc751a9
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-14 14:38:31 +0000

    [Finish] Rspec入門#10

--- spec/requests/posts_request.rb
+++ spec/requests/posts_request.rb
@@ -4,7 +4,8 @@ RSpec.describe "Posts", type: :request do
   describe "[Action test]" do
      before do
       @user = FactoryBot.create(:user, :a)
-     end
+  end
+end
 
     context "new" do
       it "access by user" do
@@ -12,13 +13,13 @@ RSpec.describe "Posts", type: :request do
         get '/posts/new'
         expect(response).to be_successful
       end
-      
+
       it "access by guest" do
         get '/posts/new'
         expect(response).to have_http_status(302)
       end
     end
-    
+
     it "show" do
       post = Post.create(
         title: "test",
@@ -28,5 +29,16 @@ RSpec.describe "Posts", type: :request do
       get post_path(post)
       expect(response).to be_successful
     end
+
+    context "create" do
+      it "access by user" do
+        sign_in @user
+        post "/posts", :params => { :post => { :title => "test", :body => "test", :user_id => 1}}
+        expect(response).to have_http_status(200)
+      end
+      it "access by guest" do
+        post "/posts"
+        expect(response).to have_http_status(401)
+      end
+    end
   end
-end
